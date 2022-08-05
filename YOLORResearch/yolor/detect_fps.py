05d16adac6747a8e9e45a87b05487f89ec0536d7
Muhammad Rizwan Munawar <62513924+RizwanMunawar@users.noreply.github.com>
2022-08-05 23:56:46 +0500

    Modification of fps module
    Modification of fps module

--- YOLORResearch/yolor/detect_fps.py
+++ YOLORResearch/yolor/detect_fps.py
@@ -105,9 +105,9 @@ def detect(save_img=False):
                 p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
             else:
                 p, s, im0 = path, '', im0s
-
-            save_path = str(Path(out) / Path(p).name)
-            txt_path = str(Path(out) / Path(p).stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
+            save_mod_path  =str(Path(out) / "yolor-")
+            save_path = str(save_mod_path+Path(p).name)
+            # txt_path = str(save_mod_path+Path(p).stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if det is not None and len(det):
